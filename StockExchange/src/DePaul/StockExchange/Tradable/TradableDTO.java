fe2c5573b7d18e89cf6fff124e3e5fbfef3b4102
vmvc2v <old.far.hot@gmail.com>
2015-04-22 17:57:24 -0500

    TradableDTO.java comments finished.

--- StockExchange/src/DePaul/StockExchange/Tradable/TradableDTO.java
+++ StockExchange/src/DePaul/StockExchange/Tradable/TradableDTO.java
@@ -18,8 +18,13 @@ package DePaul.StockExchange.Tradable;
 import DePaul.StockExchange.Price.Price;
 
 /**
- *
- * @author jimliu
+ * The TradableDTO class is based upon the “Data Transfer Object” design pattern. 
+ * The TradableDTO will act as a “data holder” that holds selected data values 
+ * from a “Tradable” object. 
+ * 
+ * @author      Junmin Liu
+ * @author      Xin Guo
+ * @author      Yuancheng Zhang
  */
 public class TradableDTO {
 
@@ -33,6 +38,27 @@ public class TradableDTO {
     public boolean isQuote;
     public String id;
     
+    /**
+     * @param productSymbol
+     * 		the product (i.e., IBM, GOOG, AAPL, etc.) that the Tradable works with
+     * @param p
+     * 		the price of the Tradable
+     * @param origVolume
+     * 		the original volume (i.e., the original quantity) of the Tradable
+     * @param remainVolume
+     * 		the remaining volume (i.e., the remaining quantity) of the Tradable
+     * @param cancelVolume
+     * 		the cancelled volume (i.e., the cancelled quantity) of the Tradable
+     * @param userName
+     * 		the User id associated with the Tradable
+     * @param s
+     * 		the “side” (BUY/SELL) of the Tradable.
+     * @param isQ
+     * 		set to true if the Tradable is part of a Quote, false if not 
+     * 		(i.e., false if it’s part of an order)
+     * @param Id
+     * 		the Tradable “id” – the value each tradable is given once it is received by the system
+     */
     public TradableDTO(String productSymbol, Price p, int origVolume, int remainVolume,
             int cancelVolume, String userName, String s, boolean isQ, String Id) {
         this.product = productSymbol;
@@ -58,6 +84,12 @@ public class TradableDTO {
         this.id = t.getId();
     }
 
+    /**
+     * Generates a recommended String value representing the key values. 
+     * Product: GE, Price: $21.59, OriginalVolume: 250, RemainingVolume: 250, 
+     * CancelledVolume: 0, User: USER1, Side: BUY, IsQuote: false, 
+     * Id: USER1GE$21.591243459303459784
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
