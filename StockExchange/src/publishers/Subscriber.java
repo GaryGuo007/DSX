c017b616ebd2f5348e1f8d4acc1574f1f4fe2a52
Yuancheng Zhang <old.far.hot@gmail.com>
2015-05-07 08:49:14 -0500

    add InvalidPublisherDataException in subscriber.java

--- StockExchange/src/publishers/Subscriber.java
+++ StockExchange/src/publishers/Subscriber.java
@@ -8,11 +8,13 @@ import client.User;
 public class Subscriber {
 	public static HashMap<String, HashSet<User>> subscriptions = new HashMap<String, HashSet<User>>();
 	
-	public synchronized void subscribe(User u, String product) throws AlreadySubscribedException { 
+	public synchronized void subscribe(User u, String product) throws AlreadySubscribedException, InvalidPublisherDataException { 
+		if(!subscriptions.keySet().contains(product)) throw new InvalidPublisherDataException("Invalid publisher data.");
 		if(subscriptions.get(product).contains(u)) throw new AlreadySubscribedException("The User has already subscribed this product.");
 		subscriptions.get(product).add(u);
 	}
-	public synchronized void unSubscribe(User u, String product) throws NotSubscribedException { 
+	public synchronized void unSubscribe(User u, String product) throws NotSubscribedException, InvalidPublisherDataException { 
+		if(!subscriptions.keySet().contains(product)) throw new InvalidPublisherDataException("Invalid publisher data.");
 		if(!subscriptions.get(product).contains(u)) throw new NotSubscribedException("The User did not subscribed this product.");
 		subscriptions.get(product).remove(u);
 	}
