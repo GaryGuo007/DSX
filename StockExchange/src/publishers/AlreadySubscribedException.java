83af491b3300a3bb72e1be1fa3fd6538345be4d8
Yuancheng Zhang <old.far.hot@gmail.com>
2015-05-07 08:31:37 -0500

    Modified 3 exp in publishers

--- StockExchange/src/publishers/AlreadySubscribedException.java
+++ StockExchange/src/publishers/AlreadySubscribedException.java
@@ -2,6 +2,9 @@ package publishers;
 
 public class AlreadySubscribedException extends Exception{
 	static final long serialVersionUID = 1;
+	public AlreadySubscribedException() {
+		super();
+	}
 	public AlreadySubscribedException(String message) {
 		super(message);
 	}
