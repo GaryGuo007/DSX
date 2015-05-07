0ea2213a66aa5a4e532e0029163b3cc196fad0fb
Yuancheng Zhang <old.far.hot@gmail.com>
2015-05-07 10:05:34 -0500

    add Singleton pattern, not finish

--- StockExchange/src/publishers/CurrentMarketPublisher.java
+++ StockExchange/src/publishers/CurrentMarketPublisher.java
@@ -1,55 +1,64 @@
 package publishers;
 
-import client.User;
+import price.Price;
+import client.*;
 
 /**
- * The CurrentMarketPublisher should implement the “Singleton” design pattern, as we only want to have a
- * single instance of the CurrentMarketPublisher. Besides the subscribe/unsubscribe functionality common
- * to all publishers, the CurrentMarketPublisher will need a method that the components of the trading
- * system will call to send the market updates out – “publishCurrentMarket”. The “publishCurrentMarket”
- * callers will provide a “MarketDataDTO” reference as a parameter to the method that contains the market
- * update data. The “publishCurrentMarket” method needs to be “synchronized” (covered later in the
- * course), as it makes use of the subscriptions HashMap - so that should be temporarily locked during use:
- * @author      Xin Guo
- * @author      Yuancheng Zhang
- * @author      Junmin Liu
+ * The CurrentMarketPublisher should implement the "Singleton" design pattern,
+ * as we only want to have a single instance of the CurrentMarketPublisher.
+ * Besides the subscribe/unsubscribe functionality common to all publishers, the
+ * CurrentMarketPublisher will need a method that the components of the trading
+ * "publishCurrentMarket" callers will provide a "MarketDataDTO" reference as a
+ * parameter to the method that contains the market update data. The
+ * "publishCurrentMarket" method needs to be "synchronized" (covered later in
+ * the course), as it makes use of the subscriptions HashMap - so that should be
+ * temporarily locked during use:
+ * 
+ * @author Xin Guo
+ * @author Yuancheng Zhang
+ * @author Junmin Liu
  */
-public class CurrentMarketPublisher {
-	public synchronized void subscribe(User u, String product) throws AlreadySubscribedException{
-		
-	}
+public final class CurrentMarketPublisher extends Subscriber {
 	
-public synchronized void unSubscribe(User u, String product) throws NotSubscribedException{
-		
-	}
+	private static CurrentMarketPublisher publishCurrentMarket;
 	
-	public synchronized void publishCurrentMarket(MarketDataDTO md){
+	public static CurrentMarketPublisher getCurrentMarketPublisher() throws Exception {
+		// TODO Auto-generated constructor stub
+		if (publishCurrentMarket == null)
+			publishCurrentMarket = new CurrentMarketPublisher();
+		return publishCurrentMarket;
+	}
+		
+	public synchronized void publishCurrentMarket(MarketDataDTO md) {
+		
 		/**
-		 * For each User object in the HashSet or ArrayList for the specified stock symbol (the stock symbol
-can be found in the MarketDataDTO), do the following:
+		 * For each User object in the HashSet or ArrayList for the specified
+		 * stock symbol (the stock symbol can be found in the MarketDataDTO), do
+		 * the following:
 		 */
+
 		/*
 		 * The String stock symbol taken from the MarketDataDTO passed in.
-        */
-		
-		
-		/* 
-		 * The BUY side price taken from the MarketDataDTO passed in. (If this is null, a Price
-		object representing $0.00 should be used, do NOT send null to the users)
 		 */
 		
 		
+
+		/*
+		 * The BUY side price taken from the MarketDataDTO passed in. (If this
+		 * is null, a Price object representing $0.00 should be used, do NOT
+		 * send null to the users)
+		 */
+
 		/*
 		 * The BUY side volume taken from the MarketDataDTO passed in.
 		 */
-		
-	
-		/*
-		 * The SELL side price taken from the MarketDataDTO passed in. (If this is null, a Price object representing $0.00 should be used, do NOT send null to the users)
 
+		/*
+		 * The SELL side price taken from the MarketDataDTO passed in. (If this
+		 * is null, a Price object representing $0.00 should be used, do NOT
+		 * send null to the users)
 		 */
-		
-		
+
 		/*
 		 * The SELL side volume taken from the MarketDataDTO passed in.
 		 */
