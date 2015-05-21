86f44bc5b81a2990c5a86231b61d6fa351bb41c5
Yuancheng Zhang <old.far.hot@gmail.com>
2015-05-21 16:32:07 -0500

    git test

--- StockExchange Phase3/src/depaul/stockexchange/messages/CancelMessage.java
+++ StockExchange Phase3/src/depaul/stockexchange/messages/CancelMessage.java
@@ -3,7 +3,6 @@ package depaul.stockexchange.messages;
 import depaul.stockexchange.BookSide;
 import depaul.stockexchange.DataValidationException;
 import depaul.stockexchange.price.Price;
-import depaul.stockexchange.tradable.Tradable;
 
 /**
  * The CancelMessage class encapsulates data related to the cancellation 
